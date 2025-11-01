11a617c9945bc37360db9cd4d573921a12d9d211
samithcsachi <samith.sachi@gmail.com>
2025-11-02 01:50:11 +0400

    EDA Stage Initialized

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import os 
 from src.config import Config
 from pathlib import Path
+import matplotlib.pyplot as plt
+from PIL import Image
+import random
+
 
 def raw_img_dir(raw_data_dir, category_names):
     image_paths = {'Fresh': [], 'Rotten': []}
@@ -22,3 +26,37 @@ def raw_img_dir(raw_data_dir, category_names):
                         image_paths[label].extend(freshness_folder.glob(ext))
                         image_paths[label].extend(freshness_folder.glob(ext.upper()))
     return image_paths
+
+
+
+
+def plot_class_distribution(class_counts, title="Class Distribution"):
+    
+    labels = list(class_counts.keys())
+    values = list(class_counts.values())
+    plt.bar(labels, values)
+    plt.title(title)
+    plt.ylabel("Image Count")
+    plt.xlabel("Class")
+    plt.show()
+
+def show_random_images(image_paths_dict, num_per_class=5, figsize=(12, 4)):
+    
+    for cls, paths in image_paths_dict.items():
+        sample_paths = random.sample(paths, min(num_per_class, len(paths)))
+        plt.figure(figsize=figsize)
+        for i, img_path in enumerate(sample_paths):
+            plt.subplot(1, len(sample_paths), i+1)
+            plt.imshow(Image.open(img_path))
+            plt.axis("off")
+            plt.title(f"{cls}")
+        plt.suptitle(f"Sample Images: {cls}")
+        plt.show()
+
+def show_image_by_path(path):
+   
+    img = Image.open(path)
+    plt.imshow(img)
+    plt.axis("off")
+    plt.title(str(path))
+    plt.show()
